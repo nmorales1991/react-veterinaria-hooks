de46fb27373af78b9b5e700d4186f33a3d799b08
Nicolás Morales <nicomorales297@gmail.com>
2019-06-22 22:20:29 -0400

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect, Fragment } from "react";
 
+
+function Cita({cita,index,eliminarCita}){
+    return(
+        <div className="cita">
+            <p>Mascota: <span>{cita.mascota}</span></p>
+            <p>Propietario: <span>{cita.propietario}</span></p>
+            <p>Fecha: <span>{cita.fecha}</span></p>
+            <p>Hora: <span>{cita.hora}</span></p>
+            <p>Síntomas: <span>{cita.sintomas}</span></p>
+
+            <button onClick={()=>eliminarCita(index)} type="button" className="button eliminar u-full-width">Eliminar X</button>
+        </div>
+    )
+
+}
+
+function Formulario({crearCita}) {
+    const stateInicial = {
+        mascota:'',
+        propietario:'',
+        fecha:'',
+        hora:'',
+        sintomas:''
+    }
+
+    const [cita, actualizarCita] = useState(stateInicial);
+
+    const actualizarState = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]:e.target.value
+        })
+    }
+    
+    const enviarCita = e =>{
+        e.preventDefault()
+
+        crearCita(cita)
+        actualizarCita(stateInicial)
+
+    }
+
+    return (
+        <Fragment>
+            <h2>Crear Cita</h2>
+
+            <form onSubmit={enviarCita}>
+                <label>Nombre Mascota</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre Mascota"
+                    onChange={actualizarState}
+                    value={cita.mascota}
+                />
+
+                <label>Nombre Dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre Dueño de la Mascota"
+                    onChange={actualizarState}
+                    value={cita.propietario}
+                />
+
+                <label>Fecha</label>
+                <input type="date" className="u-full-width" name="fecha" onChange={actualizarState} value={cita.fecha}/>
+
+                <label>Hora</label>
+                <input type="time" className="u-full-width" name="hora" onChange={actualizarState} value={cita.hora}/>
+
+                <label>Sintomas</label>
+                <textarea className="u-full-width" name="sintomas" onChange={actualizarState} value={cita.sintomas}/>
+
+                <button type="submit" className="button-primary u-full-width">
+                    Agregar
+                </button>
+            </form>
+        </Fragment>
+    );
+}
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'))
+
+    if(!citasIniciales){
+        citasIniciales=[]
+    }
+
+    const [citas, guardarCita] = useState(citasIniciales);
+
+    const crearCita = cita =>{
+        const nuevasCitas = [...citas, cita]
+        guardarCita(nuevasCitas)
+    }
+
+    const eliminarCita = index =>{
+        const nuevasCitas = [...citas]
+        nuevasCitas.splice(index,1)
+        guardarCita(nuevasCitas)
+    }
+
+    useEffect(() => {
+        let citasIniciales = JSON.parse(localStorage.getItem('citas'))
+
+        if(citasIniciales){
+            localStorage.setItem('citas',JSON.stringify(citas))
+        }else{
+            localStorage.setItem('citas',JSON.stringify([]))
+        }
+    },[citas])
+
+    const titulo = Object.keys(citas).length ===0 ? 'No hay citas':'Administrar las citas aquí'
+    return (
+        <Fragment>
+            <h1>Administrador de Pacientes</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="one-half column">
+                        <Formulario crearCita={crearCita} />
+                    </div>
+                    <div className="one-half column">
+                        <h2>{titulo}</h2>
+                        {citas.map((cita,index)=>{
+                            return <Cita
+                                eliminarCita={eliminarCita}
+                                key={index}
+                                index={index}
+                                cita={cita}
+                            />
+                        })}
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
